716fefc5999042b52c5f83b4a08df5c2fbd3ae26
NguyenMinhNhat-C41A <toilanmnhat@gmail.com>
2023-05-15 15:13:50 -0400

    Header Color

--- src/Cats.js
+++ src/Cats.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import "./Styles/Cats.css"
 export default function Cats() {
   return (
-    <div>
-      <p>Meow</p>
+    <div className = "catDiv">
+          <p>  ╱|、</p>
+          <p>(˚ˎ 。7</p>
+          <p> |   ˜〵</p>
+          <p> U Uf_)ノ</p>
     </div>
   )
 }
